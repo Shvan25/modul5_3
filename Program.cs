a9ce7777a142f15dc3b8679c028355fcc00e757d
Name <vbrfqlj@mail.ru>
2025-02-12 20:24:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Евгения";
+
+        GetName(ref name);
+        Console.WriteLine(name);
+    }
+
+    static void GetName(ref string name)
+    {
+        Console.WriteLine("Введите имя");
+        name = Console.ReadLine();
+
+    }
+}
